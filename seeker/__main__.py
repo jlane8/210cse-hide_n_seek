f87f92b0c5382d3f33bcbe670af0c571ce3c787c
jlane8 <113236803+jlane8@users.noreply.github.com>
2022-09-22 05:32:20 -0500

    added header and other comments

--- seeker/__main__.py
+++ seeker/__main__.py
@@ -1,4 +1,12 @@
+"""
+File: __main__.py
+Authors: Matt Manley (manleym@byui.edu)
+         Scott Burton (burtons@byui.edu)
+Purpose: This is the entry point into the hide and seek game.
+"""
+# import the Director class to create an instantiation of Director 
 from game.director import Director
 
+# create instantiation of Director and start the game
 director = Director()
 director.start_game()
