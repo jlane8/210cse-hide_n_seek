4bde5f36e6c15b2023024ff89b23be4b9d18fca7
jlane8 <113236803+jlane8@users.noreply.github.com>
2022-09-22 05:36:36 -0500

    added header and other comments

--- seeker/game/terminal_service.py
+++ seeker/game/terminal_service.py
@@ -1,11 +1,19 @@
-
+"""
+File: terminal_service.py
+Authors: Matt Manley (manleym@byui.edu)
+         Scott Burton (burtons@byui.edu)
+Purpose: The TerminalService class represents the I/O player interface for a
+         game of hide and seek.
+"""
+# class declaration
 class TerminalService:
     """A service that handles terminal operations.
     
     The responsibility of a TerminalService is to provide input and output operations for the 
     terminal.
     """
-     
+
+    # method to get player text input and return it to the calling Director 
     def read_text(self, prompt):
         """Gets text input from the terminal. Directs the user with the given prompt.
 
@@ -18,6 +26,7 @@ class TerminalService:
         """
         return input(prompt)
 
+    # method to get player numerical input and return it to the calling Director
     def read_number(self, prompt):
         """Gets numerical input from the terminal. Directs the user with the given prompt.
 
@@ -30,6 +39,7 @@ class TerminalService:
         """
         return float(input(prompt))
         
+    # method to simply print text to the screen for the player to read
     def write_text(self, text):
         """Displays the given text on the terminal. 
 
