b122ae890270e2131fc063d1c5262fba949e4ae5
jlane8 <113236803+jlane8@users.noreply.github.com>
2022-09-22 05:29:33 -0500

    added header and method comments

--- seeker/game/director.py
+++ seeker/game/director.py
@@ -1,7 +1,17 @@
+"""
+File: director.py
+Authors: Matt Manley (manleym@byui.edu)
+         Scott Burton (burtons@byui.edu)
+Purpose: The Director class represents the game of hide and seek, and all
+         direction that happens in it.
+"""
+# import the other custom classes to create instantiations of the TerminalService,
+# Hider, and Seeker objects
 from game.terminal_service import TerminalService
 from game.hider import Hider
 from game.seeker import Seeker
 
+# Director class represents the game and the actions in it
 class Director:
     """A person who directs the game. 
     
@@ -14,6 +24,7 @@ class Director:
         terminal_service: For getting and displaying information on the terminal.
     """
 
+    # Director constructor
     def __init__(self):
         """Constructs a new Director.
         
@@ -25,6 +36,7 @@ class Director:
         self._seeker = Seeker()
         self._terminal_service = TerminalService()
         
+    # this method runs the game until the hider is found
     def start_game(self):
         """Starts the game by running the main game loop.
         
@@ -36,6 +48,7 @@ class Director:
             self._do_updates()
             self._do_outputs()
 
+    # method to communicate with player through the TerminalService object
     def _get_inputs(self):
         """Moves the seeker to a new location.
 
@@ -45,6 +58,7 @@ class Director:
         new_location = self._terminal_service.read_number("\nEnter a location [1-1000]: ")
         self._seeker.move_location(new_location)
         
+    # method to update the hider - seeker distance difference
     def _do_updates(self):
         """Keeps watch on where the seeker is moving.
 
@@ -53,6 +67,7 @@ class Director:
         """
         self._hider.watch_seeker(self._seeker)
         
+    # method to inform player of hints, and to change game control variable if hider is found
     def _do_outputs(self):
         """Provides a hint for the seeker to use.
 
